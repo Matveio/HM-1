38b942b8bac0ec6f6ffa949d2e8bb1075e898ebd
Matveio <147967799+Matveio@users.noreply.github.com>
2023-10-19 20:11:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from random import randint
+from time import sleep
+class cat:
+
+    def __init__(self):
+
+        self.Health = 100
+        self.sleepchance = 50
+        self.meowchance = 3
+
+    def meow(self):
+        print('meow')
+        sleep(0.1)
+        print(f'Health: {self.Health} -> {self.Health + 10}')
+        self.Health += 10
+
+    def catsleep(self):
+        print('sleeping')
+        sleep(10)
+        print('sleep has end')
+        print(f'Health: {self.Health} -> {self.Health + 50}')
+        self.Health += 50
+
+
+Cat = cat()
+whiles = 0
+while True:
+    sleep(1)
+    if whiles < 20 and Cat.Health <= 200:
+        if randint(1, 3) == 2:
+            Cat.meow()
+        if randint(1, 50) == 36:
+            Cat.catsleep()
+
+        whiles += 1
+
+        print(whiles)
+
+        if Cat.Health >= 200:
+            print('your cat has won')
+            break
+        if whiles >= 20:
+            print('your cat got old')
+            break
